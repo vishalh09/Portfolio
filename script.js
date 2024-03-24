bdef5f1ccb4129dc2564044c822ec5171fbc347a
vishalh <vishalhiremath45@gmail.com>
2024-03-24 20:26:57 +0530

    commit

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove", function(e){
+  cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX
+  + "px; top: " + e.clientY + "px; ";
+})
